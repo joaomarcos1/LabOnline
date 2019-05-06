78e4f95adc02e76c00cf604db6460bcdaa125717
joaomarcos1 <rosaljoao@gmail.com>
2019-05-06 09:33:23 -0300

    Adicionando início - formulários de cadastro feitos e conectados com o sql lite 3

---
+++ labOnline/laboratorio/views.py
@@ -0,0 +1,211 @@
+from django.shortcuts import render, get_object_or_404,render_to_response, redirect
+from .models import Pessoa, Curso, Funcao, StatusArtigo, Artigo, Noticia, Evento, Area, Aluno, Professor
+
+
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from datetime import datetime
+
+
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import auth
+
+
+from .forms import RegistrationForm
+
+
+# Create your views here.
+
+def index(request):
+	noticias = Noticia.objects.all().order_by('data_lancamento_noticia').reverse()
+	return render (request, 'index.html', {'noticias':noticias})
+
+
+def login_user(request):
+	if (request.method == 'POST'):
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		print (username)
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				auth.login(request, user)
+				if request.user.is_staff:
+					return redirect('/admin')
+				else:
+					return render(request, 'interface_usuario.html')
+					#eturn redirect('interface_usuario')
+			else:
+				return render(request, 'login.html', {'error_message': 'Sua conta nao esta ativa.'})
+		else:
+			return render (request, 'login.html', {'error_message': 'Login Inválido!'})
+	return render (request,'login.html')
+
+
+
+'''
+def login_user(request):
+	if (request.method == 'POST'):
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		tipo_usuario = request.POST.get('optradio')
+		if (tipo_usuario == "Sou Professor"):
+			professor = Professor.objects.get(usuario=username, senha=password)
+			return redirect ('/interface_professor/professor.id')
+'''
+
+
+
+def interface_professor(request, id):
+	professor = Professor.objects.get(id = id)
+	return render (request, 'interface_professor.html', {'professor':professor})
+
+
+
+
+
+
+def interface_usuario(request, pk=None):
+	if not request.user.is_authenticaded():
+		return redirect('/login')
+		#return redirect('/interface_usuario/')
+	else:
+		if pk:
+			user = User.objetcs.get(pk)
+		else:
+			#user = request.user
+			#args = {'user': user}
+			args = request.user
+			return render(request, 'interface_usuario.html', {'args':args})
+	return render (request, 'interface_usuario.html')
+
+
+
+
+
+
+
+
+def logout(request):
+	auth.logout(request)
+	return redirect('/')
+
+
+
+def cadastro_usuario(request):
+	form = RegistrationForm(request.POST or None)
+	if form.is_valid():
+		user = form.save(commit=False)
+		username = form.cleaned_data['username']
+		first_name = form.cleaned_data['first_name']
+		last_name = form.cleaned_data['last_name']
+		email = form.cleaned_data['email']
+		password = form.cleaned_data['password1']
+		password2 = form.cleaned_data['password2']
+		if password != password2:
+			return render(request, 'cadastro_usuario.html')
+		elif first_name == '' or last_name == '' or email == '':
+			return render(request, 'cadastro_usuario.html')
+		user.set_password(password)
+		user.save()
+
+		if user is not None:
+			if user.is_active:
+				return redirect('\interface_usuario')
+	return render (request, 'cadastro_usuario.html', {'form':form})
+
+
+
+def cadastro_artigo(request):
+	artigos = Artigo()
+	codigo = 0
+	if(request.method == 'POST'):
+		artigos.setTitulo(request.POST.get('titulo_Artigo'))
+		artigos.setAutor(request.POST.get('autor_Artigo'))
+		artigos.setCoautor(request.POST.get('coautor_Artigo'))
+		artigos.setOrientador(request.POST.get('orientador_Artigo'))
+		artigos.setStatus(request.POST.get('status_Artigo'))
+		artigos.save()
+		codigo = 1
+		return render(request, 'cadastro_artigo.html', {'codigo':codigo})
+	return render (request, 'cadastro_artigo.html', {'codigo':codigo})
+
+
+
+
+def cadastro_aluno(request):
+	aluno = Aluno()	
+	codigo = 0
+	if (request.method == 'POST'):
+		aluno.setNome(request.POST.get('nome'))
+		aluno.setMatricula(request.POST.get('matricula'))
+		aluno.setCPF(request.POST.get('cpf'))
+		aluno.setDataNascimento(request.POST.get('data_nascimento'))
+		aluno.setCurso(request.POST.get('curso'))
+		aluno.setUsuario(request.POST.get('username'))
+		aluno.setSenha(request.POST.get('password'))
+		aluno.save()
+		codigo=1
+		return render (request, 'cadastro_aluno.html', {'codigo':codigo})
+	return render (request, 'cadastro_aluno.html', {'codigo':codigo})
+
+
+
+def cadastro_professor(request):
+	professor = Professor()
+	codigo = 0
+	if (request.method == 'POST'):
+		professor.setUsuario(request.POST.get('username'))
+		professor.setSenha(request.POST.get('password'))
+		professor.setNome(request.POST.get('nome'))
+		professor.setMatricula(request.POST.get('matricula'))
+		professor.setCPF(request.POST.get('cpf'))
+		professor.setDataNascimento(request.POST.get('data_nascimento'))
+		professor.setCurso(request.POST.get('curso'))
+		professor.setFuncao(request.POST.get('funcao'))
+		professor.setEmail(request.POST.get('email'))
+		professor.save()
+		codigo=1
+		return render (request, 'cadastro_professor.html', {'codigo':codigo})
+
+	return render (request, 'cadastro_professor.html', {'codigo':codigo})
+
+
+
+
+def cadastro_evento(request):
+	eventos = Evento()
+	area = Area()
+	codigo = 0
+	if (request.method == 'POST'):
+		eventos.setNomeEvento(request.POST.get('nome'))
+		eventos.setArea(request.POST.get('area_Evento'))
+		eventos.setDataInicioEvento(request.POST.get('data_inicio'))
+		eventos.setDataFimEvento(request.POST.get('data_fim'))
+		eventos.setQualis(request.POST.get('qualis_evento'))
+		eventos.setLocal(request.POST.get('local'))
+		eventos.setDescricao(request.POST.get('descricao_evento'))
+		eventos.save()
+		codigo = 1
+		return render(request, 'cadastro_eventos.html', {'codigo':codigo})
+	return render (request, 'cadastro_eventos.html', {'codigo':codigo})
+
+
+def cadastro_noticias(request):
+	noticia = Noticia()
+	codigo = 0
+	if (request.method == 'POST'):
+		noticia.setTitulo(request.POST.get('titulo_noticia'))
+		noticia.setCorpo(request.POST.get('corpo_noticia'))
+		noticia.setImagem(request.POST.get('imagem_noticia'))
+		noticia.setDataLancamentoNoticia(datetime.now())
+		noticia.save()
+		codigo = 1
+		return render (request, 'cadastro_noticias.html', {'codigo':codigo})
+	return render (request,'cadastro_noticias.html', {'codigo':codigo})
+
+
+def noticia(request, id):
+	noticia = Noticia.objects.get(id = id)
+	return render(request, 'noticia.html', {'noticia':noticia})
