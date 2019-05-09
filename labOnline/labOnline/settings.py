5e16d0263952e348b00d2aa8c0cb609de86e517c
joaomarcos1 <rosaljoao@gmail.com>
2019-05-09 10:40:58 -0300

    Atualizações

--- labOnline/labOnline/settings.py
+++ labOnline/labOnline/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'laboratorio',
+    'laboratorio.aluno',
+    'laboratorio.professor',
+    'laboratorio.users',
 	
 ]
 
--- labOnline/laboratorio/models.py
+++ labOnline/laboratorio/models.py
@@ -18,8 +18,6 @@ class StatusArtigo(models.Model):
 	def __str__(self):
 		return self.status
 
-
-
 class Aluno(models.Model):
 	#username = models.CharField(max_length=50,unique=True,null=True)
 	#password = models.CharField(max_length=50,null=True)
@@ -367,3 +365,36 @@ class Evento(models.Model):
 		return self.nome_evento
 
 		
+
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    username = models.CharField('Usuário', max_length=30, unique=True)
+    name = models.CharField('Nome', max_length=100, blank=True)
+    email = models.EmailField('E-mail')
+    is_staff = models.BooleanField('Equipe', default=False)
+    is_active = models.BooleanField('Ativo', default=True)
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+
+    USERNAME_FIELD = 'username's
+    REQUIRED_FIELDS = ['email']
+  
+		
+    def __str__(self):
+	    return self.name 
+
+    def get_absolute_url(self):
+	    return reverse('core:index')
+
+    class Meta:
+	    verbose_name = 'Usuário'
+	    verbose_name_plural = 'Usuários'
+	    ordering = ['id']
+
+        
